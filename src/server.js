85611a4fd44f554b83027891f266f6b18a25aa5a
Eugene Zakharchenko <eugenezaharchenko@gmail.com>
2026-04-09 23:47:24 +0300

    Scanner: release scanner with periodic GitHub release checks

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const app = require('./app');
-const db = require('./db/database');
+const { startScanner } = require('./services/scannerService');
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+startScanner();
---
+++ src/services/scannerService.js
@@ -0,0 +1,42 @@
+const db = require('../db/database');
+const { getLatestRelease } = require('./githubService');
+const { sendReleaseNotification } = require('./emailService');
+
+const INTERVAL_MS = 5 * 60 * 1000;
+
+async function checkReleases() {
+  const repos = db.prepare(`
+    SELECT DISTINCT r.id, r.owner, r.repo, r.last_seen_tag
+    FROM repositories r
+    JOIN subscriptions s ON s.repository_id = r.id
+    WHERE s.confirmed = true
+  `).all();
+
+  for (const repo of repos) {
+    const latestTag = await getLatestRelease(repo.owner, repo.repo);
+
+    if (!latestTag) continue;
+
+    if (latestTag !== repo.last_seen_tag) {
+      db.prepare('UPDATE repositories SET last_seen_tag = ? WHERE id = ?')
+        .run(latestTag, repo.id);
+
+      if (repo.last_seen_tag !== null) {
+        const subscribers = db.prepare(`
+          SELECT email FROM subscriptions
+          WHERE repository_id = ? AND confirmed = true
+        `).all(repo.id);
+
+        for (const sub of subscribers) {
+          sendReleaseNotification(sub.email, `${repo.owner}/${repo.repo}`, latestTag);
+        }
+      }
+    }
+  }
+}
+
+function startScanner() {
+  setInterval(checkReleases, INTERVAL_MS);
+}
+
+module.exports = { startScanner };
